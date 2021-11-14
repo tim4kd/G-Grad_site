87c9c51544133716938d706050d21acde52e7397
tim4kd <93346023+tim4kd@users.noreply.github.com>
2021-11-15 00:10:33 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Сайт города Г-Град</title>   
+        <link rel="stylesheet" href="main_indx.css">
+        <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+	</head>
+	<body>
+        <header>
+            <h1>Главная</h1>
+        </header>
+            <div class="block1">
+            <nav>
+                <!-- <a href="index.html">Главная</a> сылка неможет вести с главнаой на главную -->
+                <a href="news.html">Работа</a> <!-- Обмен -->
+                <a href="1.html">Законы</a> <!-- Законы -->
+                <a href="1.html">Новости</a> <!-- Новости -->
+                <a href="1.html">Билеты</a> <!-- Ставки -->
+            </nav>
+            </div>
+        <footer>
+            <h3>
+                Тут есть контакная информация
+                По всем вопросам писать суда: tim4ikd@gmail.com
+            </h3>
+        </footer>
+	</body>
+</html>
